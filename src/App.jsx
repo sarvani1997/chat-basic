61694cf06ffc11a0b136bf847cf0b2496c5b6b6e
Sarvani Sanaboyina <sarvani.sanaboyina@gmail.com>
2024-12-19 18:55:11 +0530

    basic message

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
+  const handleSendMessage = () => {
+    if(message === "") {
+      return
+    }
+    setMessage("")
+    let newList = [message].concat([...messageList]);
+
+    setMessageList(newList);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="messagesContainer">
+        <div className="list">
+          {messageList.map((msg, i) => (
+            <div
+              className={i === 0 ? "no-border":"item"}
+            >
+              <p>{msg}</p>
+            </div>
+          ))}
+        </div>
+        <div className="sendMessage">
+          <input
+            className="input"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button disabled={message === ""} onClick={handleSendMessage}>go</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
